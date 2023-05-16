27cea207d642d459d11ef59aac0d58926aa4c333
AJR <angjunray@live.com>
2023-05-16 21:23:50 +0800

    :tada: Initial Commit :D

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import WordCloud from "./wordcloud/wordcloud";
+
+// root of the app
+// renders the entire app in the div with id "root"
+ReactDOM.render(
+    <React.StrictMode>
+        <WordCloud />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
